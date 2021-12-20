30b8036a9c51ec0b4e383eece584ffd4a4076b7f
hrothwell <hrothwell@css.edu>
2021-12-19 23:06:50 -0600

    added 'no directory' directory or whatever you wanna call it

--- src/main/java/com/honeTheRat/webApp/awsWebApp/controllers/HomeController.java
+++ src/main/java/com/honeTheRat/webApp/awsWebApp/controllers/HomeController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+	@GetMapping("/")
+	public String noDirectory(Model model) {
+		model.addAttribute("name", "World");
+		return "home";
+	}
+	
 	/**
 	 * 
 	 * @param name = query paramater
