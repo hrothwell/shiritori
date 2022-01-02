28420f606145b3a31d7d5ee2c394ff9c17dabefa
hrothwell <hrothwell@css.edu>
2022-01-01 19:29:48 -0600

    following aws advice on running in background

--- awsScripts/startServer.sh
+++ awsScripts/startServer.sh
@@ -3,5 +3,5 @@ echo "starting server"
 cd /home/ec2-user/theratden/build/libs
 #show the contents for debugging
 ls
-# run as background with the "&" to prevent timeout?
-sudo java -jar ./shiritori.jar & 
+# run as background and also close things according to AWS docs
+sudo java -jar ./shiritori.jar > /dev/null 2> /dev/null < /dev/null & 
