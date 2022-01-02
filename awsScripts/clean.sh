92e8a1c9730717c4d117721d30637dbd985a5373
hrothwell <hrothwell@css.edu>
2022-01-02 13:19:41 -0600

    revise scripts to kill all 'shiritori' processes on instance

--- awsScripts/clean.sh
+++ awsScripts/clean.sh
@@ -1,4 +1,7 @@
 # delete the app folder if it already exists
 echo "removing theratden folder"
 cd /home/ec2-user
+#TODO I don't know if this really works?
 sudo rm -rf theratden
+#Force to kill all "shiritori" processes, which should hopefully just be my jar
+sudo kill -9 (ps -ef | grep shiritori | grep -v grep | awk '{print $2}')
